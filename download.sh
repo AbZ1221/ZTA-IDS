7556156913143ef70dba4aeb6f3faff9e39e0e0b
AbZ1221 <abeer.alalmaie@gmail.com>
2023-11-09 13:18:19 +0330

    Download the dataset

---
+++ download.sh
@@ -0,0 +1,5 @@
+# Downloading the data
+!wget -O UNSW_NB15_training-set.csv "https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2FUNSW-NB15%20-%20CSV%20Files%2Fa%20part%20of%20training%20and%20testing%20set&files=UNSW_NB15_training-set.csv"
+!wget -O UNSW_NB15_testing-set.csv "https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2FUNSW-NB15%20-%20CSV%20Files%2Fa%20part%20of%20training%20and%20testing%20set&files=UNSW_NB15_testing-set.csv"
+!wget -O UNSW-NB15_features.csv "https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2FUNSW-NB15%20-%20CSV%20Files&files=NUSW-NB15_features.csv&downloadStartSecret=ylo8ggjacdr"
+
